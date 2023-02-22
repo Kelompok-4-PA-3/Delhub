2990a2e756cf09374391c9e31388848a3da0c282
Rizki24-star <kikingar.567@gmail.com>
2023-02-22 15:40:38 +0700

    blade-template

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('auth.login');
-// });
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    // Route::get('/', function () {
-    //     return view('auth.login');
-    // })->name('login');
+    Route::get('/dashboard', function () {
+        return view('index');
+    })->name('dashboard');
 });
