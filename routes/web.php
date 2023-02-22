34ea63f4e161ba6eb429a777d9572ceafd6fc2b5
Rizki24-star <kikingar.567@gmail.com>
2023-02-22 14:38:03 +0700

    autentikasi

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    // Route::get('/', function () {
+    //     return view('auth.login');
+    // })->name('login');
 });
