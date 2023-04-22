4d6cef846ca2985cb47b209556814ac83bf174fd
marloxxx <66661134+marloxxx@users.noreply.github.com>
2023-04-22 21:36:11 +0700

    hot fix

--- app/Http/Resources/RequestResource.php
+++ app/Http/Resources/RequestResource.php
@@ -18,7 +18,7 @@ class RequestResource extends JsonResource
         return [
             'id' => $this->id,
             'ruangan' => $this->ruangan->nama,
-            'deskripsi' => $this->deskripsi ?? '-',
+            'deskripsi' => $this->description ?? '-',
             'waktu' => Carbon::parse($this->waktu)->format('d-m-Y H:i'),
             'status' => $this->reference->value,
         ];
